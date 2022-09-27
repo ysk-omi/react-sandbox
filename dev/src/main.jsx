01a7e04e4395c0ce2676bd3cc256ee235ed59071
yusuke omi <ysk.omi0818@gmail.com>
2022-09-27 16:42:06 +0900

    propTypes

--- dev/src/main.jsx
+++ dev/src/main.jsx
@@ -1,39 +1,50 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import PropTypes from "prop-types";
+import { createRoot } from "react-dom/client";
 
 class HeaderComponent extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      val: 'Heading 1'
-    }
-    this.handleInputChange = (e) => this._handleInputChange(e)
-    this.handleButtonClick = () => this._handleButtonClick()
+      val: "Heading 1",
+    };
+    this.handleInputChange = (e) => this._handleInputChange(e);
+    this.handleButtonClick = () => this._handleButtonClick();
   }
   _handleInputChange(e) {
-    this.setState({val: e.target.value})
+    this.setState({ val: e.target.value });
   }
   _handleButtonClick() {
-    this.setState({val: ''})
+    this.setState({ val: "" });
   }
   render() {
     let { val } = this.state;
     return (
       <div>
-        <input
-          type="text"
-          onChange={this.handleInputChange}
-          value={val}
-        />
-        <button
-          type="button"
-          onClick={this.handleButtonClick}
-        >Clear</button>
-        <h1>
-          {val}
-        </h1>
+        <input type="text" onChange={this.handleInputChange} value={val} />
+        <button type="button" onClick={this.handleButtonClick}>
+          Clear
+        </button>
+        <h1>{val}</h1>
       </div>
-    )
+    );
+  }
+}
+
+class TextComponent extends React.Component {
+  render() {
+    let bool = true;
+    return (
+      <div>
+        {(() => {
+          if (bool) {
+            return "Good Morning";
+          } else {
+            return "Good Evening";
+          }
+        })()}
+      </div>
+    );
   }
 }
 
@@ -44,12 +55,32 @@ class PropsComponent extends React.Component {
     return (
       <div>
         <HeaderComponent></HeaderComponent>
-        Hello {target}!
+        Hello {target}!<TextComponent></TextComponent>
       </div>
     );
   }
 }
 
-const $app = document.getElementById('app');
-const root = createRoot($app)
-root.render(<PropsComponent target="Every one" />);
+class Greeting extends React.Component {
+  // コンポーネントが実際のDOMに追加された後に実行
+  componentDidMount() {
+    console.log(window.navigator);
+  }
+  render() {
+    const { isBirthday } = this.props;
+
+    return <div>{isBirthday ? "Happy Birthday" : "Hello how are you?"}</div>;
+  }
+}
+
+Greeting.propTypes = {
+  isBirthday: PropTypes.bool,
+};
+
+Greeting.defaultProps = {
+  isBirthday: true,
+};
+
+const $app = document.getElementById("app");
+const root = createRoot($app);
+root.render(<Greeting />);
